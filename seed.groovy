93add77150a88b259f7919248ee35d74c491cb75
shivarkzs <shivarkzs>
2021-05-19 18:02:55 +0530

    main..

--- seed.groovy
+++ seed.groovy
@@ -8,7 +8,7 @@ def component = ["frontend","users","login","todo"];
 def count=(component.size()-1)
 for (i in 0..count) {
   def j=component[i]
-  pipelineJob("CI-Pipelines/${j}-ci") {
+  pipelineJob('CI-Pipelines/frontend-ci') {
     configure { flowdefinition ->
       flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
