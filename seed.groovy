fcbb9d1fb6780370ef9b4fc854d6ab683e2d801b
shivarkzs <shivarkzs>
2021-05-19 18:04:53 +0530

    commioted

--- seed.groovy
+++ seed.groovy
@@ -14,7 +14,7 @@ for (i in 0..count) {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/shivarkzs/frontend.git'+j+'.git')
+              'url'('https://github.com/shivarkzs/frontend.git')
             }
           }
           'branches' {
