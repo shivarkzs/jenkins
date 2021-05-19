6c9a360ef03d87f6ec59e0d2e12a7110a606e87c
shivarkzs <shivarkzs>
2021-05-19 22:07:32 +0530

    changes

--- seed.groovy
+++ seed.groovy
@@ -1,14 +1,4 @@
-folder('CI-Pipelines') {
-  displayName('CI Pipelines')
-  description('CI Pipelines')
-}
-
-def component = ["frontend","users","login","todo"];
-
-def count=(component.size()-1)
-for (i in 0..count) {
-  def j=component[i]
-  pipelineJob('CI-Pipelines/frontend-ci') {
+  pipelineJob('frontend-ci') {
     configure { flowdefinition ->
       flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
