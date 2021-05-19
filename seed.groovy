d0cdb573d3f79158454704da53ad9f07de3d9afc
shivarkzs <shivarkzs>
2021-05-19 17:52:55 +0530

    scm1

--- seed.groovy
+++ seed.groovy
@@ -1,20 +1,32 @@
-pipelinejob('frontend-ci') {
-    configure {flowdefinition ->
-    floedefinition << delegate. 'definition'(class:'org.jenkinsci-plugins.workflow.cps.CpsScmFlowDefinition' ,plugin: 'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GITSCM:git) {
-        'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-                'url ('https://github.com/shivarkzs/frontend.git.)
-            }
-        }
-        'branches' {
-            'hudson.plugin.git.BranchSpec' {
-                'name' ('*/main')
-            }
-        }
-    }
-    'ScriptPath'('jenkinsfile')
-    'lightweight'(true)
-    }
- }
+folder('CI-Pipelines') {
+  displayName('CI Pipelines')
+  description('CI Pipelines')
 }
+
+def component = ["frontend","users","login","todo"];
+
+def count=(component.size()-1)
+for (i in 0..count) {
+  def j=component[i]
+  pipelineJob("CI-Pipelines/${j}-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/shivarkzs/frontend.git'+j+'.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+}
+
