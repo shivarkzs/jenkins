1fcdd4b048b9917603c225845fc895a17c951221
shivarkzs <shivarkzs>
2021-05-21 18:29:46 +0530

    log

--- seed.groovy
+++ seed.groovy
@@ -24,4 +24,25 @@
     }
   }
 
+  pipelineJob('CI-pipelines/login-ci') {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/shivarkzs/login.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+
 
