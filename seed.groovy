abdc77cab9d6cbe7192e3c955077da4d852a4639
shivarkzs <shivarkzs>
2021-05-24 12:07:02 +0530

    123...

--- seed.groovy
+++ seed.groovy
@@ -1,4 +1,4 @@
- folder('CI-pipelines') {
+ { flowfolder('CI-pipelines') {
     displayName('CI pipelines')
     description('CI pipelines')
 }
@@ -31,6 +31,27 @@
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
               'url'('https://github.com/shivarkzs/login.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+
+   pipelineJob('CI-pipelines/users-ci') {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/shivarkzs/users.git')
             }
           }
           'branches' {
