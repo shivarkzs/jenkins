8ce83c6207949476ea172afdb7b5e90e47c89475
shivarkzs <shivarkzs>
2021-05-24 12:49:56 +0530

    new

--- seed.groovy
+++ seed.groovy
@@ -1,36 +1,21 @@
-    flowfolder('CI-pipelines') {
-    displayName('CI pipelines')
-    description('CI pipelines')
+     
+folder('CI-Pipelines') {
+  displayName('CI Pipelines')
+  description('CI Pipelines')
 }
 
-  pipelineJob('CI-pipelines/frontend-ci') {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/shivarkzs/frontend.git')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
-    }
-  }
+def component = ["frontend","users","login","todo"];
 
-  pipelineJob('CI-pipelines/login-ci') {
+def count=(component.size()-1)
+for (i in 0..count) {
+  def j=component[i]
+  pipelineJob("CI-Pipelines/${j}-ci") {
     configure { flowdefinition ->
       flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/shivarkzs/login.git')
+              'url'('https://github.com/shivarkzs/'+j+'.git')
             }
           }
           'branches' {
@@ -44,26 +29,6 @@
       }
     }
   }
-
-   pipelineJob('CI-pipelines/users-ci') {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/shivarkzs/users.git')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
-    }
-  }
+}
 
 
